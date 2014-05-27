cf29ee14556984772d5a6a66f06f6fd2966da0be
arovillard <arovillard@gmail.com>
2014-05-26 21:27:53 -0400

    emails sent informing users they guessed right

--- app/admin/game.rb
+++ app/admin/game.rb
@@ -22,25 +22,28 @@ ActiveAdmin.register Game do
   end
 
   controller do
-    after_filter :check_winner, only: [:edit, :create]
+    def update(options={}, &block)
+      # This is taken from the active_admin code
+      super do |success, failure|
+        block.call(success, failure) if block
+        failure.html { render :edit }
 
-    private
-
-    def check_winner
-      if @game.team_a_result != nil && @game.team_b_result != nil
-        current_winners = [0]
-        current_winners += Winner.where("game_id = ?", @game.id).pluck(:user_id)
-        new_winners = Entry.where("game_id = ? and team_a_score = ? and team_b_score = ? and user_id not in (?)",
-                    @game.id, @game.team_a_result, @game.team_b_result, current_winners)
-        new_winners.each do |winner|
-          taken = [0]
-          raffle_number = 0
-          taken += Game.find(winner.game_id).winners.pluck(:raffle_number)
-          (raffle_number = rand(1..1000)) until taken.include?(raffle_number) == false
-          var = Winner.new(:user_id => winner.user_id, :entry_id => winner.id, :game_id => winner.game_id, :raffle_number => raffle_number)
-          var.save
-          @user = User.find(winner.user_id)
-          WinnerMailer.winner_email_2.deliver
+        if @game.team_a_result != nil && @game.team_b_result != nil
+          current_winners = [0]
+          current_winners += Winner.where("game_id = ?", @game.id).pluck(:user_id)
+          new_winners = Entry.where("game_id = ? and team_a_score = ? and team_b_score = ? and user_id not in (?)",
+                      @game.id, @game.team_a_result, @game.team_b_result, current_winners)
+          new_winners.each do |winner|
+            taken = [0]
+            raffle_number = 0
+            taken += Game.find(winner.game_id).winners.pluck(:raffle_number)
+            (raffle_number = rand(1..1000)) until taken.include?(raffle_number) == false
+            var = Winner.new(:user_id => winner.user_id, :entry_id => winner.id, :game_id => winner.game_id, :raffle_number => raffle_number)
+            var.save
+            @raffle_number = raffle_number
+            @user = User.find(winner.user_id)
+            WinnerMailer.winner_email_template(@user, @game, @raffle_number).deliver
+          end
         end
       end
     end
--- app/mailers/winner_mailer.rb
+++ app/mailers/winner_mailer.rb
@@ -7,7 +7,10 @@ class WinnerMailer < ActionMailer::Base
   #   mail(from: "from@example.com", to: user.email, subject: 'Your guess was right!!')
   # end
 
-  def winner_email_2
+  def winner_email_template (user, game, raffle_number)
+    @user = user
+    @game = game
+    @raffle_number = raffle_number
     mail(from: "from@example.com", to: "hello@hello.com", subject: "hello world")
   end
 end
