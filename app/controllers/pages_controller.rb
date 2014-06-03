852fdcb99f8bbd1e5b2c85dc939d78fa40cc0cbd
arovillard <arovillard@gmail.com>
2014-06-03 18:42:07 -0400

    mis apuestas

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,8 @@ class PagesController < ApplicationController
   before_action :authenticate_user!
 
   def user
+    @user = current_user
+    @entries = Entry.where(:user_id => current_user)
 
   end
 
