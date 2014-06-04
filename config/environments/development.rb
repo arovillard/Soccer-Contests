b0772b094c610ea760e13599dfdd1ed1d49f8554
arovillard <arovillard@gmail.com>
2014-06-04 09:06:42 -0400

    action mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,9 +1,19 @@
 Wcc::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   #mailer
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'wcc-staging.herokuapp.com' }
+
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {:address => "localhost", :port => 1025}
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => 'baci.lindsaar.net',
+    :user_name            => 'arovillard@gmail.com',
+    :password             => 'Pavillon2236',
+    :authentication       => 'plain',
+    :enable_starttls_auto => true  }
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {:address => "localhost", :port => 1025}
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
